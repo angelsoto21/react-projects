995fcac48cb854c6c0508b7dbbdcf9acd1d44fe3
angel soto <angelsotto21@gmail.com>
2023-07-05 05:42:59 -0700

    detalles segundo projects

--- projectsreact/01-tic-tac-toe/src/App.jsx
+++ projectsreact/01-tic-tac-toe/src/App.jsx
@@ -6,15 +6,15 @@ const Turns = {
 };
 
 const ganador = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 const Square = ({ children, updateBoard, index, isSelected }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
@@ -32,47 +32,58 @@ const Square = ({ children, updateBoard, index, isSelected }) => {
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(Turns.X);
-  const [winner, setWinner] = useState(null)
+  const [winner, setWinner] = useState(null);
 
   const checkwinner = (boardToCheck) => {
-    for(const combo of ganador) {
-      const [a,b,c] = combo
-      if(
-        boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]
+    for (const combo of ganador) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
       ) {
-        return boardToCheck[a]
+        return boardToCheck[a];
       }
     }
     // si no hay ganador
-    return null
-  }
+    return null;
+  };
+  const checkEndGame = (board) => {
+    return board.every((square) => square !== null);
+  };
 
   const updateBoard = (index) => {
-    //para no sobre escribir 
-    if(board[index] || winner) return
+    //para no sobre escribir
+    if (board[index] || winner) return;
 
-    //actualizar el tablero 
-    const newBoard = [...board]
-    newBoard[index] = turn
-    setBoard(newBoard)
+    //actualizar el tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
 
     const newTurn = turn === Turns.X ? Turns.O : Turns.X;
     setTurn(newTurn);
 
     //revisar si hay ganador
     // para hacer un console log no puedes poner el estado debes de poner el newWinner en vez del Winner
-    const newWinner = checkwinner(newBoard)
+    const newWinner = checkwinner(newBoard);
     if (newWinner) {
-      setWinner(newWinner)
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
     }
+  };
 
+  const Reiniciar = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(Turns.X);
+    setWinner(null);
   };
 
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
       <section className="game">
-        
         {board.map((_, index) => {
           return (
             <Square key={index} index={index} updateBoard={updateBoard}>
@@ -87,7 +98,19 @@ function App() {
         <Square isSelected={turn === Turns.O}>{Turns.O}</Square>
       </section>
 
-      
+      {winner !== null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner === false ? "Empate" : "Gano:"}</h2>
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+            <footer>
+              <button onClick={Reiniciar}>Empezar de nuevo</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
